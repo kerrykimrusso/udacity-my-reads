ebbff3125654f9e6c08069077141d73acbc6eca0
Kerry <kerry.kim.russo@gmail.com>
2017-09-02 17:41:49 -0400

    feat: use mock api to render 3 lists based on shelf property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
-import './App.css';
+import * as BooksAPI from './service/BooksAPI.mock';
+import 'semantic-ui-css/semantic.min.css';
+import List from './component/SemanticUiList';
+import BookListItem from './component/BookListItem'
+import ShelfEnum from './enum/shelf.enum';
 
 class App extends Component {
+  state = {
+    books: []
+  }
+
+  componentWillMount = () => {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState((prevState) => {
+          return {
+            books
+          }
+        })
+      });
+  }
+  
+  getCurrentlyReading = () => this.state.books.filter((book) => book.shelf === ShelfEnum.CURRENTLY_READING);
+
+  getWantToRead = () => this.state.books.filter((book) => book.shelf === ShelfEnum.WANT_TO_READ);
+
+  getRead = () => this.state.books.filter((book) => book.shelf === ShelfEnum.READ);
+
+  createBookListItemFromBook = (book) => <BookListItem 
+    key={book.id}
+    id={book.id}
+    imageSrc={book.imageLinks.smallThumbnail}
+    title={book.title}
+    subtitle={book.subtitle}
+    authors={book.authors}
+    description={book.description} 
+    shelfControl={''}
+  />;
+
   render() {
+    const listClasses = ['ui', 'items', 'unstackable'];
+
+    let currentlyReading = this.getCurrentlyReading().map(this.createBookListItemFromBook);
+    let wantToRead = this.getWantToRead().map(this.createBookListItemFromBook);
+    let read = this.getRead().map(this.createBookListItemFromBook);
+    
     return (
       <div className="app">
-        
+        <List classes={listClasses} items={currentlyReading} />
+        <List classes={listClasses} items={wantToRead} />
+        <List classes={listClasses} items={read} />
       </div>
     );
   }
