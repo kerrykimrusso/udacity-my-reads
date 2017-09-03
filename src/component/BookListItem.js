0158103139e4bd3c900189b4d995f62e7a07add6
Kerry <kerry.kim.russo@gmail.com>
2017-09-02 20:18:03 -0400

    refactor: remove description from props; add segment class to item

--- src/component/BookListItem.js
+++ src/component/BookListItem.js
@@ -8,34 +8,30 @@ export class BookListItem extends Component {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string,
     authors: PropTypes.arrayOf(PropTypes.string),
-    description: PropTypes.string.isRequired,
     shelfControl: PropTypes.node,
   }
 
   render() {
-    let {imageSrc, title, subtitle, authors, description, shelfControl} = this.props;
+    let {imageSrc, title, subtitle, authors, shelfControl} = this.props;
     subtitle = subtitle ? `: ${subtitle}` : '';
     authors = authors.join(', ');
 
     return (
-      <div className='item'>
-        <div className='image'>
-            <img src={imageSrc} alt={`${title}${subtitle} by ${authors}`}/>
-        </div>
-        <div className='content'>
-            <h1 className='header'>{`${title}${subtitle}`}</h1>
-            <div className='meta'>
-                <span>{authors}</span>
+        <div className='ui item segment'>
+            <div className='ui tiny image'>
+                <img src={imageSrc} alt={`${title}${subtitle} by ${authors}`}/>
             </div>
-            <div className='description'>
-                <p>{description}</p>
-            </div>
-            <div className='extra'>
-                {shelfControl}
+            <div className='content'>
+                <h1 className='header'>{`${title}${subtitle}`}</h1>
+                <div className='meta'>
+                    <span>{authors}</span>
+                </div>
+                <div className='extra'>
+                    {shelfControl}
+                </div>
             </div>
         </div>
-      </div>
-    )
+    );
   }
 }
 
