6c4dd00370af242793b320bb49af0ee697cf3d2b
Kerry <kerry.kim.russo@gmail.com>
2017-09-05 01:24:43 -0400

    add api call to update books shelf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
-import * as BooksAPI from './service/BooksAPI.mock';
+import * as BooksAPI from './service/BooksAPI';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import HomePage from './page/home.page';
@@ -29,11 +29,28 @@ class App extends Component {
             shelf: toShelf
           };
 
-          if(id === book.id) return Object.assign({}, book, updatedBook);
-          return book;
-        })
+  changeShelf = (id, toShelf) => {
+    let bookToUpdate;
+    const books = this.state.books.map((book) => {
+      const updatedBook = {
+        shelf: toShelf
+      };
+
+      if(id === book.id) {
+        bookToUpdate = book;
+        return Object.assign({}, book, updatedBook);
+      }
+
+      return book;
+    });
+
+    this.setState((prevState) => {
+      return {
+        books
       };
     });
+
+    BooksAPI.update(bookToUpdate, toShelf);
   }
 
   render() {
